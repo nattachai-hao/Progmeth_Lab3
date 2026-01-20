31dc1944e97d75cbb61d635e3cda416c724c4ba2
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 00:28:08 +0700

    card

--- src/main/java/card/base/Card.java
+++ src/main/java/card/base/Card.java
@@ -1,24 +1,30 @@
 package card.base;
 
 //You CAN modify the first line
-public class  Card implements Cloneable {	
+public abstract class  Card implements Cloneable {
+	protected String name;
+	protected String flavorText;
+	protected int bloodCost;
 
 	/// You can modify code below ///
-	public Card(String name, String flavorText, int bloodCost) {	
+	public Card(String name, String flavorText, int bloodCost) {
+		this.name = name;
+		this.flavorText = flavorText;
+		this.bloodCost = bloodCost;
 	}
 		
-	public String toString();
+	public abstract String toString();
 	
 	public String getName() {
-
+		return name;
 	}
 
 	public int getBloodCost() {
-
+		return bloodCost;
 	}
 
 	public String getFlavorText() {
-
+		return flavorText;
 	}
 	
 	/// You can modify code above ///
