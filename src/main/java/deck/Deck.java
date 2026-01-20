32186d28e407a23e61b37d5024c825a9797a34f0
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 00:17:28 +0700

    removeCard

--- src/main/java/deck/Deck.java
+++ src/main/java/deck/Deck.java
@@ -7,83 +7,92 @@ import card.base.Card;
 
 
 public class Deck {
-	protected String name;
-	protected int deckSize;
-	protected Card[] deckList;
-	// TODO: constructor
-	
-	public Deck(String name, Card[] deckList) {
-		setName(name);
-		setDeckList(deckList);
-		setDeckSize(deckList.length);
-	}
+    protected String name;
+    protected int deckSize;
+    protected Card[] deckList;
+    // TODO: constructor
 
-	//You CAN modify the first line
-		public int insertCard(Card card) throws InsertCardFailedException{
-			int count = 0;
-			//หาการ์ดซ้ำ
-			for(Card e : deckList) {
-				if(e.equals(card)) {
-					count++;
-				}
-			}
-			if(count >= 4) 
-				throw new InsertCardFailedException("You can only put 4 of the same cards into the deck");
-			//FILL CODE HERE
-			//You can use Arrays.copyOf(Original Array, New Length) to create new arrays with bigger size
-			//Must return new deckSize
-			Card[] newDeck = Arrays.copyOf(deckList,deckList.length+1);
-			newDeck[newDeck.length-1] = card;
-			setDeckSize(newDeck.length);
-			setDeckList(newDeck);
-			return getDeckSize();
-		}
+    public Deck(String name, Card[] deckList) {
+        setName(name);
+        setDeckList(deckList);
+        setDeckSize(deckList.length);
+    }
 
-		//You CAN modify the first line
-		public Card removeCard(int slotNumber) throws RemoveCardFailedException {
-			if (this.deckList.length <= slotNumber) {
-				throw new RemoveCardFailedException("Number you insert exceed deck size");
-			}		
-			if (this.deckList[slotNumber] == null) {
-				throw new RemoveCardFailedException("There is no card in that slot");
-			}
-			//FILL CODE HERE
-			//You can use Arrays.copyOf(Original Array, New Length) to create new arrays with bigger size (Added slot is empty)
-			//Once card is removed, other card down the list must rearrange to the empty slot
-			//Must return card that was removed
-		}
+    //You CAN modify the first line
+    public int insertCard(Card card) throws InsertCardFailedException {
+        int count = 0;
+        //หาการ์ดซ้ำ
+        for (Card e : deckList) {
+            if (e.equals(card)) {
+                count++;
+            }
+        }
+        if (count >= 4)
+            throw new InsertCardFailedException("You can only put 4 of the same cards into the deck");
+        //FILL CODE HERE
+        //You can use Arrays.copyOf(Original Array, New Length) to create new arrays with bigger size
+        //Must return new deckSize
+        Card[] newDeck = Arrays.copyOf(deckList, deckList.length + 1);
+        newDeck[newDeck.length - 1] = card;
+        setDeckSize(newDeck.length);
+        setDeckList(newDeck);
+        return getDeckSize();
+    }
 
-	@Override
-	public String toString() {
-		return new StringBuilder()
-				.append("{").append(this.getName()).append("}")
-				.append("(").append(this.getDeckSize()).append(" deck size)")
-				.toString();
-	}
+    //You CAN modify the first line
+    public Card removeCard(int slotNumber) throws RemoveCardFailedException {
+        if (this.deckList.length <= slotNumber) {
+            throw new RemoveCardFailedException("Number you insert exceed deck size");
+        }
+        if (this.deckList[slotNumber] == null) {
+            throw new RemoveCardFailedException("There is no card in that slot");
+        }
+        //FILL CODE HERE
+        //You can use Arrays.copyOf(Original Array, New Length) to create new arrays with bigger size (Added slot is empty)
+        //Once card is removed, other card down the list must rearrange to the empty slot
+        //Must return card that was removed
+        Card[] newDeck = Arrays.copyOf(deckList, deckList.length + 1);
+        Card removeCard = newDeck[slotNumber];
+        for(int i = slotNumber; i < newDeck.length - 1; i++) {
+            newDeck[i] = newDeck[i+1];
+        }
+        Card[] finalDeck = Arrays.copyOf(newDeck,newDeck.length - 1);
+        setDeckList(finalDeck);
+        setDeckSize(finalDeck.length);
+        return removeCard;
+    }
 
-	/* GETTERS & SETTERS */
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("{").append(this.getName()).append("}")
+                .append("(").append(this.getDeckSize()).append(" deck size)")
+                .toString();
+    }
 
-	public String getName() {
-		return name;
-	}
+    /* GETTERS & SETTERS */
 
-	public int getDeckSize() {
-		return deckSize;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Card[] getDeckList() {
-		return deckList;
-	}
+    public int getDeckSize() {
+        return deckSize;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Card[] getDeckList() {
+        return deckList;
+    }
 
-	public void setDeckSize(int deckSize) {
-		this.deckSize = deckSize;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setDeckList(Card[] deckList) {
-		this.deckList = deckList;
-	}
+    public void setDeckSize(int deckSize) {
+        this.deckSize = deckSize;
+    }
+
+    public void setDeckList(Card[] deckList) {
+        this.deckList = deckList;
+    }
 }
