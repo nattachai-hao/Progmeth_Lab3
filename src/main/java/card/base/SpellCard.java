46d3bd03264c0893714744588bac73e68be4a226
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 00:41:48 +0700

    SpellCard

--- src/main/java/card/base/SpellCard.java
+++ src/main/java/card/base/SpellCard.java
@@ -1,18 +1,28 @@
 package card.base;
 
 //You CAN modify the first line
-public class SpellCard extends Card {
+public abstract class SpellCard extends Card {
+    protected boolean isBurstSpeed;
 
+    public SpellCard(String name, String flavorText, int bloodCost, boolean isBurstSpeed) {
+        super(name, flavorText, bloodCost);
+        setBurstSpeed(isBurstSpeed);
+    }
 
-	public SpellCard(String name, String flavorText, int bloodCost, boolean isBurstSpeed) {
+    public abstract void castSpell(UnitCard unitCard);
 
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return this.getName() + "\tCost: " + this.getBloodCost() +
-				"\t(" + (this.getFlavorText()) + ")";
-	}
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return this.getName() + "\tCost: " + this.getBloodCost() +
+                "\t(" + (this.getFlavorText()) + ")";
+    }
 
+    public boolean isBurstSpeed() {
+        return isBurstSpeed;
+    }
+
+    public void setBurstSpeed(boolean burstSpeed) {
+        isBurstSpeed = burstSpeed;
+    }
 }
