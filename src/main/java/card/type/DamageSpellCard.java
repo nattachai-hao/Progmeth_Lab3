676010b8fb3aca44d1dc24c510c4c6f5b1bce510
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 09:40:15 +0700

    DamageSpellCard

--- src/main/java/card/type/DamageSpellCard.java
+++ src/main/java/card/type/DamageSpellCard.java
@@ -5,10 +5,27 @@ import card.base.UnitCard;
 
 //You CAN modify the first line
 public class DamageSpellCard extends SpellCard {
-	
-	public DamageSpellCard(String name, String flavorText, int bloodCost, 
-			boolean isBurstSpeed, int damage) {
+    private int damage;
 
-	}
+    public DamageSpellCard(String name, String flavorText, int bloodCost,
+                           boolean isBurstSpeed, int damage) {
+        super(name, flavorText, bloodCost, isBurstSpeed);
+        setDamage(damage);
+    }
 
+    public void castSpell(UnitCard unitCard) {
+        unitCard.setHealth(unitCard.getHealth() - getDamage());
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        if (damage < 1) {
+            this.damage = 1;
+        } else {
+            this.damage = damage;
+        }
+    }
 }
