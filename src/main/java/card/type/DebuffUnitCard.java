4cd7fbf76e7c82bf3eb563dbf7078bc75fe2a0db
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 09:15:39 +0700

    DebuffUnitCarad:

--- src/main/java/card/type/DebuffUnitCard.java
+++ src/main/java/card/type/DebuffUnitCard.java
@@ -4,11 +4,31 @@ import card.base.UnitCard;
 import player.Player;
 
 //You CAN modify the first line
-public class DebuffUnitCard extends UnitCard {	
-	
-	public DebuffUnitCard(String name, String flavorText, int bloodCost, int power, int health, int debuffPower) {
+public class DebuffUnitCard extends UnitCard {
+    protected int debuffPower;
 
-	}	
-	
-	
+    public DebuffUnitCard(String name, String flavorText, int bloodCost, int power, int health, int debuffPower) {
+        super(name, flavorText, bloodCost, power, health);
+        setDebuffPower(debuffPower);
+    }
+
+    public int attackUnit(UnitCard unitCard) {
+        if (this.power > unitCard.getHealth()) {
+            unitCard.setHealth(0);
+            unitCard.setPower(unitCard.getPower() - getDebuffPower());
+            return unitCard.getHealth();
+        } else {
+            unitCard.setHealth(unitCard.getHealth() - this.power);
+            unitCard.setPower(unitCard.getPower() - getDebuffPower());
+            return this.power;
+        }
+    }
+
+    public int getDebuffPower() {
+        return debuffPower;
+    }
+
+    public void setDebuffPower(int debuffPower) {
+        this.debuffPower = debuffPower;
+    }
 }
