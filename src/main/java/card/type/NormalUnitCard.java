26bebfae28edb348166a5794dcacef0b3387ec50
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 09:10:28 +0700

    NormalUnitCard

--- src/main/java/card/type/NormalUnitCard.java
+++ src/main/java/card/type/NormalUnitCard.java
@@ -5,11 +5,19 @@ import player.Player;
 
 //You CAN modify the first line
 public class NormalUnitCard extends UnitCard {
-	
-	public NormalUnitCard(String name, String flavorText, int bloodCost, int power, int health) {
-		
-	}
-	
-	
+
+    public NormalUnitCard(String name, String flavorText, int bloodCost, int power, int health) {
+        super(name, flavorText, bloodCost, power, health);
+    }
+
+    public int attackUnit(UnitCard unitCard) {
+        if (this.power > unitCard.getHealth()) {
+            unitCard.setHealth(0);
+            return unitCard.getHealth();
+        } else {
+            unitCard.setHealth(unitCard.getHealth() - this.power);
+            return this.power;
+        }
+    }
 }
 
