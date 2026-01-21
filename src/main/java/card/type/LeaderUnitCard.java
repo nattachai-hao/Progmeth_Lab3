898d8902e2595aae4877727801909a3e62b8d7ce
Nattachai_Hao <Nattachai.hao@gmail.com>
2026-01-21 09:31:48 +0700

    LeaderUnitCard

--- src/main/java/card/type/LeaderUnitCard.java
+++ src/main/java/card/type/LeaderUnitCard.java
@@ -5,15 +5,54 @@ import player.Player;
 
 //You CAN modify the first line
 public class LeaderUnitCard extends UnitCard {
-	
-	public LeaderUnitCard(String name, String flavorText, int bloodCost, int power, int health, int buffPower, int buffHealth) {
+    protected int buffPower;
+    protected int buffHealth;
 
-	}
-	
-	@Override
-	public String toString() {
-		return super.getName() + " (POW: " + super.getPower() + ", HP: " + super.getHealth() + 
-				" | POW Inc: "+ this.getBuffPower() + ", HP Inc: " + this.getBuffHealth() + ")";
-	}
+    public LeaderUnitCard(String name, String flavorText, int bloodCost, int power, int health, int buffPower, int buffHealth) {
+        super(name, flavorText, bloodCost, power, health);
+        setBuffPower(buffPower);
+        setBuffHealth(buffHealth);
+    }
+
+    public int attackUnit(UnitCard unitCard) {
+        if (this.power > unitCard.getHealth()) {
+            int realDamage = unitCard.getHealth();
+            unitCard.setHealth(0);
+            return realDamage;
+        } else {
+            unitCard.setHealth(unitCard.getHealth() - this.power);
+            return this.power;
+        }
+    }
+
+    public void buffUnit(UnitCard[] alliesCard) {
+        for (int i = 0; i < alliesCard.length; i++) {
+            if (alliesCard[i].equals(null)) continue;
+            alliesCard[i].setPower(alliesCard[i].getPower() + getBuffPower());
+            alliesCard[i].setHealth(alliesCard[i].getHealth() + getBuffHealth());
+        }
+    }
+
+    public int getBuffPower() {
+        return buffPower;
+    }
+
+    public int getBuffHealth() {
+        return buffHealth;
+    }
+
+    public void setBuffPower(int buffPower) {
+        this.buffPower = buffPower;
+    }
+
+    public void setBuffHealth(int buffHealth) {
+        this.buffHealth = buffHealth;
+    }
+
+    @Override
+    public String toString() {
+        return super.getName() + " (POW: " + super.getPower() + ", HP: " + super.getHealth() +
+                " | POW Inc: " + this.getBuffPower() + ", HP Inc: " + this.getBuffHealth() + ")";
+    }
 
 }
--- src/main/java/card/type/VenomUnitCard.java
+++ src/main/java/card/type/VenomUnitCard.java
@@ -5,19 +5,19 @@ import player.Player;
 
 //You CAN modify the first line
 public class VenomUnitCard extends UnitCard {
-	
-	public VenomUnitCard(String name, String flavorText, int bloodCost, int power, int health) {
-		super(name, flavorText, bloodCost, power, health);
-	}
 
-	public int attackUnit(UnitCard unitCard) {
-		int realDamage = unitCard.getHealth();
-		unitCard.setHealth(0);
-		return realDamage;
-	}
+    public VenomUnitCard(String name, String flavorText, int bloodCost, int power, int health) {
+        super(name, flavorText, bloodCost, power, health);
+    }
 
-	public int dead(Player player) {
-		player.takeDamage(getPower());
-		return getPower();
-	}
+    public int attackUnit(UnitCard unitCard) {
+        int realDamage = unitCard.getHealth();
+        unitCard.setHealth(0);
+        return realDamage;
+    }
+
+    public int dead(Player player) {
+        player.takeDamage(getPower());
+        return getPower();
+    }
 }
